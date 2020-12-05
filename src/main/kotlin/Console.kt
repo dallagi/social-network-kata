341a824e62825e0aab978bab362d80b21a3be186
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-05 18:44:10 +0100

    Make feature test pass

--- src/main/kotlin/Console.kt
+++ src/main/kotlin/Console.kt
@@ -1,5 +1,5 @@
 package dev.dallagi.socialnetwork
 
-interface Console {
+fun interface Console {
     fun printLine(message: String)
 }
--- src/test/kotlin/PersonalTimelineFeature.kt
+++ src/test/kotlin/PersonalTimelineFeature.kt
@@ -1,28 +1,41 @@
-import dev.dallagi.socialnetwork.Console
-import dev.dallagi.socialnetwork.SocialNetwork
+import dev.dallagi.socialnetwork.*
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verifySequence
 import org.junit.jupiter.api.Test
+import java.time.Instant
 
 class PersonalTimelineFeature {
+    private val fixedClock = Clock { now }
+    private val console = mockk<Console>(relaxed = true)
+    private val postMessageClock = mockk<Clock>(relaxed = true)
+    private val messagesRepository = InMemoryMessagesRepository()
+    private val readTimeline = ReadTimeline(NMinutesAgoTimeHumanizer(fixedClock), messagesRepository, console)
+    private val postMessage = PostMessage(messagesRepository, postMessageClock)
+
     @Test
     fun `users can submit messages to personal timelines`() {
-        val socialNetwork = SocialNetwork(emptyList())
-        val console = mockk<Console>(relaxed = true)
+        val socialNetwork = SocialNetwork(listOf(postMessage, readTimeline))
+        every { postMessageClock.now() }.returnsMany(fiveMinutesAgo, twoMinutesAgo, oneMinuteAgo)
 
         socialNetwork.send("Alice -> I love the weather today")
         socialNetwork.send("Bob -> Damn! We lost!")
         socialNetwork.send("Bob -> Good game though.")
 
         socialNetwork.send("Alice")
+        socialNetwork.send("Bob")
+
         verifySequence {
             console.printLine("I love the weather today (5 minutes ago)")
-        }
-
-        socialNetwork.send("Bob")
-        verifySequence {
             console.printLine("Good game though. (1 minute ago)")
             console.printLine("Damn! We lost! (2 minutes ago)")
         }
+    }
+
+    companion object {
+        private val now = Instant.parse("2020-10-10T10:10:10Z")
+        private val oneMinuteAgo = Instant.parse("2020-10-10T10:09:10Z")
+        private val twoMinutesAgo = Instant.parse("2020-10-10T10:08:10Z")
+        private val fiveMinutesAgo = Instant.parse("2020-10-10T10:05:10Z")
     }
 }
